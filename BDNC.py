eceff0c93cd5fa0d2fea76edb93c9193cf065594
MUHIB-143 <122245772+MUHIB-143@users.noreply.github.com>
2023-01-09 17:53:15 +0600

    Update BDNC.py

--- BDNC.py
+++ BDNC.py
@@ -10,13 +10,13 @@ if bit == '64bit':
 
     print("\033[1;32m[•] \033[1;33mCONGRATULATIONS YOUR DEVICE SUPPORT THE  TOOL\033[1;35m ")
 
-    from MUHIB-143 import Main
+    from MUHIB-64 import Main
 
     Main()
 
 elif bit == '32bit':
     print("\033[1;32m[•] \033[1;33mCONGRATULATIONS YOUR DEVICE SUPPORT THE  TOOL\033[1;35m ")
-    from MUHIB-143o import Main
+    from MUHIB-143 import Main
     Main()
 
        
